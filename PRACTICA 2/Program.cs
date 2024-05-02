96593677656bd3271a0864f969935a9c153d09e2
kurisuST <crisaronbb59@gmail.com>
2024-05-01 18:58:08 -0600

    Agregar archivos de proyecto.

---
+++ PRACTICA 2/Program.cs
@@ -0,0 +1,9 @@
+﻿int num, num1, num2, num3,num4;
+
+Console.WriteLine("Write a 3 digit number");
+num = int.Parse(Console.ReadLine());
+num1 = num % 10;
+num3= num / 100;
+num2 = num / 10;
+num4 = num2 % 10;
+Console.WriteLine("Number inversion: "+num1 + num4 + num3);
